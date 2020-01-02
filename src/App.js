370ed550b8810ccfc06476c2f61d7063399d9d69
peter-boot <peter.boot@gmail.com>
2020-01-02 03:31:34 +0000

    eddited App.js 12

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Changed on Feature branch
+         Running on t2.micro
         </p>
         <a
           className="App-link"
