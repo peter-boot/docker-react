17b66789c1681d5dd76c512ca080de48dba35836
peter-boot <peter.boot@gmail.com>
2020-01-02 02:53:30 +0000

    eddited App.js 11

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload 24.
+         Changed on Feature branch
         </p>
         <a
           className="App-link"
